7de3132bb7a9e51b81cfb2f611022bdb1f6b44d2
Andy Dwelly <andy.dwelly@digital.hmrc.gov.uk>
2018-03-09 08:35:07 +0000

    VOAS-82: WIP

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -10,13 +10,17 @@ object MicroServiceBuild extends Build with MicroService {
   val appName = "voa-bar"
   private val mockitoAllVersion = "1.10.19"
   private val scalaTestPlusPlayVersion = "2.0.1"
+  private val httpCachingClientVersion = "7.0.0"
+  private val playReactivemongoVersion = "6.0.0"
 
   override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
 
   val compile = Seq(
     "uk.gov.hmrc" %% "play-reactivemongo" % "6.1.0",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.4.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.4.0",
+    "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
+    "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion
   )
 
   def test(scope: String = "test,it") = Seq(
