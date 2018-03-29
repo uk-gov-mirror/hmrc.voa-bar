cbe104b45d8d4bd2fc026d04b9bbeecf20179423
Geoff Watson <geoff.watson@digital.hmrc.gov.uk>
2018-03-29 13:27:45 +0100

    VOAS-13: additional tests

--- test/uk/gov/hmrc/voabar/services/ValidationServiceSpec.scala
+++ test/uk/gov/hmrc/voabar/services/ValidationServiceSpec.scala
@@ -21,7 +21,7 @@ import org.scalatestplus.play.PlaySpec
 import play.api.test.FakeRequest
 import uk.gov.hmrc.voabar.models.Error
 
-import scala.xml.{Node, NodeSeq, XML}
+import scala.xml.{Node, XML}
 
 class ValidationServiceSpec extends PlaySpec {
 
@@ -52,7 +52,7 @@ class ValidationServiceSpec extends PlaySpec {
       validationService("9999").validate(validBatch).isEmpty mustBe true
     }
 
-    "return an empty list (no errors) when passed a valid batch with 42 reports" in {
+    "return an empty list (no errors) when passed a valid batch with 32 reports" in {
       val validBatch:Node = XML.loadString(batchWith32Reports)
       validationService("5243").validate(validBatch).isEmpty mustBe true
     }
@@ -130,7 +130,22 @@ class ValidationServiceSpec extends PlaySpec {
 
     "return a list of errors when a batch containing 32 reports has multiple errors" in {
       val invalidBatch = XML.loadString(batchWith32ReportsWithErrors)
-      validationService("5243").validate(invalidBatch.head).size mustBe 19
+      validationService("8888").validate(invalidBatch.head) mustBe List(
+        Error("1010",List("BAIdentityCode", "BA code in request header does not match with that in the batch report")),
+        Error("cvc-complex-type.2.4.a",List("Invalid content was found starting with element 'IllegalElement'. One of " +
+          "'{\"http://www.govtalk.gov.uk/LG/Valuebill\":EntryDateTime}' is expected.")),
+        Error("cvc-datatype-valid.1.2.1",List("'0£' is not a valid value for 'integer'.")),
+        Error("cvc-type.3.1.3",List("The value '0£' of element 'TotalNNDRreportCount' is not valid.")),
+        Error("1000",List("Trailer", "IllegalElement", "Some text")),
+        Error("1000",List("Trailer", "TotalNNDRreportCount", "0£")),
+        Error("1000",List("138161", "StreetDescription", "23 NeW ST")),
+        Error("cvc-complex-type.2.4.a",List("Invalid content was found starting with element 'BadElement'. " +
+          "One of '{\"http://www.govtalk.gov.uk/LG/Valuebill\":TypeOfTax}' is expected.")),
+        Error("1000",List("138156", "Town", "WIMBLETOWN£")),
+        Error("cvc-complex-type.2.4.a",List("Invalid content was found starting with element 'ExistingEntries'. " +
+          "One of '{\"http://www.govtalk.gov.uk/LG/Valuebill\":ProposedEntries, \"http://www.govtalk.gov.uk/LG/Value" +
+          "bill\":IndicatedDateOfChange}' is expected.")),
+        Error("1007",List("138159", "CR09")))
     }
   }
 }
